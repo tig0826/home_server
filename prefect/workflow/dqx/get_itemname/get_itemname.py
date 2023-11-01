791c0baab22ae930b6991b28cfceba11b49efa0c
kamahori <52903669+tig0826@users.noreply.github.com>
2023-11-01 09:11:38 +0900

    Prefect dqx get itemname flow (#10)
    * get itemname first deploy
    
    最初のデプロイを作成。アイテム名のスクレイピングを実行しているが、まだ不安定な部分がある
    ひらがなとかたかなで網羅的にアイテム名を収集しているので難しい。
    漢字しか含まないアイテムは収集できないので、剣や石などよくある漢字を指定して検索。(常用漢字すべてで検索は時間がかかるので却下)
    
    * modify get itemname workflow
    
    アイテム取得のワークフローを作成した。
    しかし、検索対象に対して100件までしか取得できなかったため、全てのアイテム名を取得するのは難しい。
    そこで、一般サイトのアイテムデータベースからワンショットで取得し、
    新しいアイテムが追加された場合に手動で更新することにする。
    アイテムデータベースで現在もリアルタイムに更新されているサイトは今のところなさそうなので、
    差分は自分で取得する必要がある...
    
    * create workflow get_item_name
    
    - 公式サイトからは全てのアイテム名を取得できなかったので、別のサイトからワンショットで取得
    - postgresqlに登録も完了している
    
    * delete chrome driver from child directory
    
    下の階層のディレクトリそれぞれにchrome driverを配置するのは冗長なので、上の階層のworkflowにのみ配置する形式にした。
    
    * create get itemname hash
    
    - 公式サイトで価格を検索する時にいちいちseleniumでアイテムを検索するのは時間がかかるので、予めhashを取得して直接検索結果に飛べるようにした

--- prefect/workflow/dqx/get_itemname/get_itemname.py
+++ prefect/workflow/dqx/get_itemname/get_itemname.py
@@ -1,184 +1,110 @@
 #a   -*- coding: utf-8 -*-
 
-import openpyxl
-import logging
 import pandas as pd
+from sqlalchemy.util import warn_exception
 from prefect import flow, task
 import psycopg2
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from sqlalchemy import create_engine
 import time
 from time import sleep
 from prefect_github.repository import GitHubRepository
 from prefect.blocks.system import Secret
 import datetime
+import requests
+from bs4 import BeautifulSoup
 
 
 @task(retries=3)
-def load_git_repo():
-    github_repository_block = GitHubRepository.load("github-dqx-get-itemname")
-
-
-@task(retries=3)
-def login_dqx():
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    options.add_argument("enable-automation")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-infobars")
-    options.add_argument('--disable-extensions')
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-browser-side-navigation")
-    options.add_argument("--disable-gpu")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    prefs = {"profile.default_content_setting_values.notifications" : 2}
-    options.add_experimental_option("prefs",prefs)
-    # webdriverのパスを指定
-    service = Service(executable_path="chromedriver-mac-arm64/chromedriver")
-    driver = webdriver.Chrome(service=service,options=options)
-    # 検索先のURL
-    driver.get('https://hiroba.dqx.jp/sc/search/')
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'sqexid')))
-    secret_block_user = Secret.load("dqx-user")
-    dqx_user = secret_block_user.get()
-    secret_block_passwd = Secret.load("dqx-passwd")
-    dqx_passwd = secret_block_passwd.get()
-    # 検索窓入力
-    s = driver.find_element(By.XPATH,'//*[@id="sqexid"]')
-    s.send_keys(dqx_user)
-    s = driver.find_element(By.XPATH,'//*[@id="password"]')
-    s.send_keys(dqx_passwd)
-    # ログインボタンクリック
-    driver.find_element(By.XPATH,'//*[@id="login-button"]').click()
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'welcome_box')))
-    driver.find_element(By.XPATH,'//*[@id="welcome_box"]/div[2]/a').click()
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'contentArea')))
-    driver.find_element(By.XPATH,'//*[@id="contentArea"]/div/div[2]/form/table/tbody/tr[2]/td[3]/a').click()
-    # 検索
-    return driver
-
-
-@task(retries=10)
-def search_item(driver, search_word):
-    # 出品情報を格納するリスト
-    exhibition_data = []
-    #すべての文字で検索
-    # 検索するアイテム名を格納
-    print(f"search {search_word}")
-    # 検索フォームに入力
-    time.sleep(2)
-    try:
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'searchword')))
-    except TimeoutException as e:
-        driver.find_element(By.XPATH, '//*[@id="historyBack"]').click()
-        logging.error(f"TimeoutException occurred: {e}. Element with ID 'searchword' was not found")
-    s = driver.find_element(By.XPATH, '//*[@id="searchword"]').clear()
-    time.sleep(1)
-    s = driver.find_element(By.XPATH, '//*[@id="searchword"]')
-    time.sleep(3)
-    s.send_keys(search_word)
-    time.sleep(1)
-    try:
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'searchBoxArea')))
-    except TimeoutException as e:
-        driver.find_element(By.XPATH, '//*[@id="historyBack"]').click()
-        logging.error(f"TimeoutException occurred: {e}. Element with ID 'searchBoxArea' was not found")
-    driver.find_element(By.XPATH, '//*[@id="searchBoxArea"]/form/p[2]/input').click()
-    try:
-        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, 'searchTabItem')))
-    except TimeoutException as e:
-        driver.find_element(By.XPATH, '//*[@id="historyBack"]').click()
-        logging.error(f"TimeoutException occurred: {e}. Element with ID 'searchTabItem' was not found")
-    time.sleep(3)
-    driver.find_element(By.XPATH, '//*[@id="searchTabItem"]').click()
-    time.sleep(3)
-    print("find_elements item list")
-    while(True):
-        # 出品データの取得
-        time.sleep(3)
-        elements = driver.find_elements(By.TAG_NAME, 'tr')
-        # リストに格納
-        for elem in elements:
-            exhibition_data.append(elem.text.split())
-        try:
-            WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
-        except TimeoutException as e:
-            driver.find_element(By.XPATH, '//*[@id="historyBack"]').click()
-            logging.error(f"TimeoutException occurred: {e}. Element with ID 'next' was not found")
-        if len(driver.find_elements(By.XPATH, '//*[@class="next"]')) > 0:
-            print("- click next")
-            driver.find_element(By.XPATH, '//*[@class="next"]').click()
-        else:
-            break
-    return exhibition_data
-
+def search_item(base_url,url_hashes):
+    item_list = []
+    for item_type, u in url_hashes.items():
+        item_name = ""
+        pagenum=1
+        while item_name != "No Results Found!":
+            url = base_url.format(u=u,pagenum=pagenum)
+            print(url)
+            response = requests.get(url)
+            response.encoding = response.apparent_encoding
+            bs = BeautifulSoup(response.text, "html.parser")
+            for row in bs.find_all('tr'):
+                cells = row.find_all('td')
+                item_name = cells[0].getText().strip()
+                print(item_name)
+                if item_name != "No Results Found!":
+                    item_list.append([item_name,item_type])
+            sleep(0.3)
+            pagenum += 1
+    df = pd.DataFrame(item_list, columns=["アイテム名", "カテゴリ"])
+    return df
 
 @task(retries=3)
-def deduplicate_item_list(exhibition_data):
-    # アイテム名の重複など無駄な要素を削除する
-    exhibition_data = [i for i in exhibition_data if len(i) > 1 and i[0] != "アイテム名"]
-    return exhibition_data
-
-
-@task(retries=3)
-def separate_soubi_dougu(exhibition_data):
-    print('-- separate soubi and dougu ---')
-    df_item = pd.DataFrame(exhibition_data)
-    df_item = df_item.drop_duplicates().reset_index(drop=True)
-    is_dougu = df_item.iloc[:, 4] == "装備可能な職業はありません"
-    df_dougu = df_item[is_dougu].reset_index(drop=True)
-    df_dougu = df_dougu.iloc[:, [0, 1]]
-    df_dougu.columns = ["item_name", "category"]
-    df_soubi = df_item[~ is_dougu].reset_index(drop=True)
-    df_soubi = df_soubi.iloc[:, [0, 1, 3]]
-    df_soubi.columns = ["item_name", "category", "level"]
-    return df_dougu, df_soubi
-
-
-@task(retries=3)
-def save_to_postgresql(df_dougu, df_soubi):
+def save_to_postgresql(df, table_name):
     # 収集したデータを保存
     print('-- save to postgresql ---')
+    secret_block_postgresql_passwd = Secret.load("postgresql-tig-passwd")
+    postgresql_passwd = secret_block_postgresql_passwd.get()
     connection_config = {
             "user": "tig",
-            "password": "0826tattkmhr",
+            "password": postgresql_passwd,
             "host": "postgresql.mynet.local",
             "port": "5432",
             "dbname": "dqx"}
     engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format(**connection_config))
-    df_dougu.to_sql('dougu_name', con=engine, if_exists='replace', index=False)
-    df_soubi.to_sql('soubi_name', con=engine, if_exists='replace', index=False)
-
+    df.to_sql(table_name, con=engine, if_exists='replace', index=False)
 
 @flow(log_prints=True)
 def get_itemname():
-    load_git_repo()
-    driver = login_dqx()
-    exhibition_data = []
-    hiragana_exclude = ['ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ', 'っ', 'ゃ', 'ゅ', 'ょ', 'ゎ', 'ゐ', 'ゑ']
-    hiragana = [chr(i) for i in range(12353, 12436) if chr(i) not in hiragana_exclude]
-    katakana_exclude = ['ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ッ', 'ャ', 'ュ', 'ョ', 'ヮ' ]
-    katakana = [chr(i) for i in range(12449, 12532+1) if chr(i) not in katakana_exclude]
-    kanji = ["石", "玉", "剣", "盾", "杖", "弓", "券", "書", "天", "竜", "草", "竹", "超"]
-    for search_word in hiragana:
-        exhibition_data += search_item(driver, search_word)
-    for search_word in katakana:
-        exhibition_data += search_item(driver, search_word)
-    for search_word in kanji:
-        exhibition_data += search_item(driver, search_word)
-    exhibition_data = deduplicate_item_list(exhibition_data)
-    df_dougu, df_soubi = separate_soubi_dougu(exhibition_data)
-    save_to_postgresql(df_dougu, df_soubi)
-    driver.quit()
+    weapon_base_url = "https://dqx-souba.game-blog.app/equip/{u}?min_shuppin=0&max_shuppin=600&max_lv=120&min_lv=1&sortColumn=lv&sortDirection=desc&page={pagenum}"
+    weapon_url_hashes = {
+            "片手剣" : "5efced945477a85bc95818712ee21a7ead69dc79",
+            "両手剣" : "f5ae932a9a1c29df422fb1ace2fbf47b04fb8b22",
+            "短剣" : "060670c72defc85aa5617558304d80709df433c4",
+            "槍" : "6d74ee269f99d56a9cc6d232908d46db1616b627",
+            "斧" : "e43b64fcb698a5a621df2804f9497d3b6de102da",
+            "爪" : "62e63d700ac5f945f6fa7d5c186af99382da22ba",
+            "鞭" : "f16e1c8dd6ec2edb5bbd71e7baab20b5d43bef0d",
+            "スティック" : "8c62ace4fc6584e77043e067337ea47bcd1473d9",
+            "両手杖" : "eac13cd87919b844bbd03c7e923962b924eed38c",
+            "棍" : "2f2fdbed31f8efa5889adc07e21af697e069bb63",
+            "扇" : "af17d80dc1b100c5cd5d013ec49d53e113c870e8",
+            "ハンマー" : "aa5b1c0e7ffff8e14f305d298307d80a06cfdde7",
+            "弓" : "d89b5a73d0fa96a0327b20bfa8a3b74aa1a8010c",
+            "ブーメラン" : "52289762a19be43f6fca58ad5cf9529127421aaa",
+            "鎌" : "c7784660f019b039a59c83e40bcd4264c31ea51f"
+            }
 
+    armor_base_url = "https://dqx-souba.game-blog.app/equip/{u}?min_shuppin=0&max_shuppin=600&max_lv=120&min_lv=1&sortColumn=lv&sortDirection=desc&page={pagenum}"
+    armor_urls_hashes = {
+            "頭" : "8b4ffbf6743e78e19dbd7907fddcff70822b6022",
+            "体上" : "30212e6c5e9d731b0631927e340ec113487111c7",
+            "体下" : "0b23ded64e49d3f61d356525c35d3ebd18fe9c2c",
+            "腕" : "186ec38d3ee521c2b9cb930a15b46954553cabb5",
+            "足" : "506c43307dfedececc21317de1eb870ec6690e88",
+            "盾" : "bb2d8b75f236e4f753ac13d3d7f2ae698a307896"
+            }
+
+
+    dougu_base_url = "https://dqx-souba.game-blog.app/item/{u}?min_shuppin=0&max_shuppin=600&subcategory=all&sortColumn=lv&sortDirection=desc&bazaar=false&drop_item=false&n_drop_item=false&page={pagenum}"
+    dougu_urls_hashes = {
+            "素材" : "7aa9b827f1185a011e67191405869c2165447040",
+            "消費アイテム" : "7459fb532a1d69a54091d951b2623242335bf46b",
+            "印章" : "9b64c10654cd7a846b6b98cc415ca4e17b2c557b",
+            "レシピ" : "1524543f24a0e6505e01f3409c0992258511d384",
+            "料理" : "3323a1b5c53af813752fb6e9f253d6e3a7a2c014",
+            "しぐさ書" : "7936501f8f4b40fbfc2c9112be7a96ca0398f43a",
+            "スカウトの書" : "aa9d09cdd083b9bf07ff587a45d269707ff93920",
+            "ポーズ書" : "a592f11567a3327e84a4fbb2dc225f2908dcc8c2",
+            "タネ" : "31cd09a4fa3d2530b9437b44f9553bc3ee20afcd",
+            "釣りざお" : "3da56db203ffa9c5b6eaebde15cb07622ccd4103",
+            "ルアー" : "6c9eac07f2d64fec2d7eeee689000952beccf668"
+            }
+
+    df_weapon = search_item(weapon_base_url, weapon_url_hashes)
+    df_armor = search_item(armor_base_url, armor_urls_hashes)
+    df_dougu = search_item(dougu_base_url, dougu_urls_hashes)
+    save_to_postgresql(df_weapon, "name_weapon")
+    save_to_postgresql(df_armor, "name_armor")
+    save_to_postgresql(df_dougu, "name_dougu")
 
 if __name__ == "__main__":
     # get_itemname.serve(name="get_itemname",cron="0 1 15 * *")
